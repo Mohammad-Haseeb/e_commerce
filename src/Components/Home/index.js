44eb6b9aebc0da88e9457d91c7569d70a68d95c1
Mohammad-Haseeb <mohammad.haseeb310@gmail.com>
2021-03-08 02:14:43 +0500

    registration button add in main Componet

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -2,9 +2,22 @@ import React from 'react';
 import  Style from  './index.module.css'
 import logo from './logo.jpg';
 import emoji from './emoji.png';
+import Button from '@material-ui/core/Button';
+
 
 
 export  let FirstMain=()=>{
+    return(
+        <>
+          <FirstComponent/>
+          <div className={Style.RegisterButton}>
+          <RegisterBtn/>
+               </div>
+        </>
+    );
+}
+
+let FirstComponent=()=>{
     return(
         <>
         <div className={Style.Main}>
@@ -20,7 +33,18 @@ export  let FirstMain=()=>{
            </div>
 
         </div>
-
         </>
     );
+}
+
+let  RegisterBtn=()=>{
+   return(
+       <>
+        <Button variant="contained" className={Style.rbuton} 
+         style={{ backgroundImage:
+      'linear-gradient( 986deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)'}}>
+        Registration
+    </Button>
+       </>
+   );
 }
